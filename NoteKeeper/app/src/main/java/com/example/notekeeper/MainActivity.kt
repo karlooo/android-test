a0005a016c64dba4254ac08d414465a284bb1ec4
Karlo Cagay <P1013A@ISD-PF0XDES0.gdcusaadc.com>
2019-06-21 00:57:19 +0800

    NoteKeeper

---
+++ NoteKeeper/app/src/main/java/com/example/notekeeper/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.notekeeper
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var notePosition = POSITION_NOT_SET
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val adapterCourses = ArrayAdapter<CourseInfo>(this,
+            android.R.layout.simple_spinner_item,
+            DataManager.courses.values.toList())
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCourses.adapter = adapterCourses
+
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+
+        if ( notePosition != POSITION_NOT_SET )
+        {
+            displayNote()
+        }
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            R.id.action_next -> {
+                moveNext()
+                true
+            }
+            R.id.action_previous -> {
+                movePrevious()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if ( notePosition >= DataManager.notes.lastIndex )
+        {
+            val menuItem = menu?.findItem(R.id.action_next)
+            if ( menuItem != null )
+            {
+                menuItem.isEnabled = false
+                menuItem.icon = getDrawable(R.drawable.ic_chevron_right_black_24dp)
+            }
+        }
+
+        if ( notePosition == 0 )
+        {
+            val menuItem = menu?.findItem(R.id.action_previous)
+            if ( menuItem != null )
+            {
+                menuItem.isEnabled = false
+                menuItem.icon = getDrawable(R.drawable.ic_chevron_left_black_24dp)
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    private fun movePrevious() {
+        notePosition--
+        displayNote()
+        invalidateOptionsMenu()
+    }
+
+    private fun moveNext() {
+        notePosition++
+        displayNote()
+        invalidateOptionsMenu()
+    }
+}
